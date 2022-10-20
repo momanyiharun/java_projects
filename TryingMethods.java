f3046ce26c83d4e819f89452a84755cd0c4576b5
Harun Momanyi <harun.momanyi@gmail.com>
2022-10-20 09:51:23 +0000

    final addition

---
+++ TryingMethods.java
@@ -0,0 +1,39 @@
+//This is me trying to practise methods in Java
+public class TryingMethods {
+ 
+   public static void main(String[] args) {
+    
+    }   
+
+   String MainMethod(int day) {
+    String dayOfTheWeek;
+    switch (day) {
+        case  1:
+        dayOfTheWeek = "Sunday";
+        break;
+        case 2: 
+        dayOfTheWeek = "Monday";
+        break;
+        case 3:
+        dayOfTheWeek = "Tuesday";
+        break;
+        case 4:
+        dayOfTheWeek = "Wednesday";
+        break;
+        case 5:
+        dayOfTheWeek = "Thursday";
+        break;
+        case 6:
+        dayOfTheWeek = "Friday";
+        break;
+        case 7:
+        dayOfTheWeek = "Saturday";
+        break;
+        default:
+            break;
+
+    }
+    return dayOfTheWeek;  
+    
+
+}
