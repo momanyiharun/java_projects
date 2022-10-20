5a5228a56b8ef79920737dc2b20f3e33fd4bf166
Harun Momanyi <harun.momanyi@gmail.com>
2022-10-20 10:15:40 +0000

    This is a summation function

---
+++ SummationOne.java
@@ -0,0 +1,16 @@
+public class SummationOne {
+    public static void main(String[] args) {
+
+    }
+
+    static int sumFiveHundred(){
+        int sum = 0, i=1;
+        while(i<500){
+            sum+=i;
+            i++;
+        }
+        return sum;
+    }
+
+    }
+        
